f2d93408beab1b28583c97cc911c953973ab078d
Mammad Mammadli <memmed199@gmail.com>
2024-02-02 00:31:58 +0400

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Mammad Mammadli on 02.02.24.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
